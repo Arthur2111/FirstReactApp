a0c276821bdb1912c40a1d9f417e32a0dabb6a76
Arthur2111 <khongweihwang1997@gmail.com>
2022-12-29 15:20:37 +0800

    attaching event handlers ,using functions and accesseing events in functions while passing arguments

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,25 @@
 const Home = () => {
-    return ( 
+
+    // this is how we can define a function and assign it to a button
+    // vue stores all functions as methods within its script tag 
+    // since React uses State Component Functions we use JSX where we can write JS with HTML together
+    const handleClick = (e) => {
+        console.log('hello i am a blogger', e)
+    }
+
+    const handleClickAgain = (name,e) => {
+        console.log('hello ' + name , '----------', e)
+    }
+
+    return (
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            {/* how to pass arguments? we wrap our main function in an anonymus function */}
+            {/* if we do not wrap our functions it will instantly be invokes */}
+            <button onClick={(e) => handleClickAgain('Hwang', e) }> Click me again</button>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
-<div className="home">
-    <h2>HomePage</h2>
-</div>
